228450d74a23db4522afa05bbd538c9e2ede8c7f
Nicolas-R-Dev <232324422+Nicolas-R-Dev@users.noreply.github.com>
2026-06-01 13:19:53 +0200

    fix: remove Next.js redirect, let Vercel handle www to non-www

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.poker-reflex.com' }],
-        destination: 'https://poker-reflex.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
